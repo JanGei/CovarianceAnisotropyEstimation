corrected error calculation - assimilating only every 4th step

main/dependencies/model_params.py
@@ -100,6 +100,7 @@ def get():
     q_idx       = [5, 9, 15, 27, 31]
     mask        = np.full(len(well_loc),True,dtype=bool)
     mask[q_idx] = False
+    years = 2
     
     cov_mods    = ['Exponential', 'Matern', 'Gaussian']
     computer = ['office', 'binnac']
@@ -112,7 +113,8 @@ def get():
         up_temp = True
         inspection = False
         n_pre_run = 5
-        printf = False
+        printf = True
+        asimdays = [2*years, 315*years]
     elif setup == 'binnac':
         n_mem  = 140
         nprocs = psutil.cpu_count()
@@ -120,6 +122,7 @@ def get():
         n_pre_run = 20
         printf = False
         inspection = False
+        asimdays = [25*years, 315*years]
     
     
     choice = [0, 1]
@@ -130,12 +133,13 @@ def get():
     nPP = 30
     
     conditional_flag = True
+    
     pilot_point_even = False
     scramble_pp = False
     
     h_damp = 0.15
-    cov_damp = 0.1
-    npf_damp = 0.1
+    cov_damp = 0.2
+    npf_damp = 0.2
     damp = [[h_damp, cov_damp, npf_damp], [h_damp, cov_damp], [h_damp, npf_damp]]
     
     
@@ -168,6 +172,7 @@ def get():
         'nearPP': 4,
         'sig_me': 0.1,
         'geomea': 0.1,
+        'years' : years,
         'condfl': conditional_flag,
         'covt'  : covtype,
         'valt'  : valtype,
@@ -186,8 +191,8 @@ def get():
         'welxy' : np.array(well_loc[q_idx]),                # location of pumps
         'obsxy' : np.array(well_loc[mask]),                 # location of obs
         'welq'  : np.array([35, 18, 90, 20, 15])/3600,      # Q of wells [m3s-1]
-        'welst' : np.array([20, 300, 200, 0, 0]),           # start day of pump
-        'welnd' : np.array([150, 365, 360, 370, 300]),      # end day of pump
+        'welst' : np.array([20, int(315*years), 200, 0, 0]),# start day of pump
+        'welnd' : np.array([150, 365, 365, 200, 300])*years,# end day of pump
         'welay' : np.array(np.zeros(5)),                    # layer of wells
         'river' : np.array([[0.0,0], [5000,0]]),            # start / end of river
         'rivC'  : 5*1e-4,                                   # river conductance [ms-1]
@@ -196,6 +201,7 @@ def get():
         'chdh'  : 15,                                       # initial stage of riv
         'ss'    : 1e-5,                                     # specific storage
         'sy'    : 0.15,                                     # specific yield
+        'asim_d': asimdays,
         'mname' : "Reference",
         'sname' : "Reference",
         'inspec': inspection,
@@ -218,7 +224,7 @@ def get():
         'tm_ws' : os.path.join(ensemb_dir, 'template_model'),
         'trs_ws': os.path.join(Vrdir, 'transient_model') ,
         'resdir': os.path.join(parent_directory, 'output'),
-        'nsteps': int(365*24/6),
+        'nsteps': int(years*365*24/6),
         'nprern': n_pre_run,
         'rotmat': rotation_matrix,
         'mat2cv': extract_truth,

main/main.py
@@ -150,18 +150,18 @@ if __name__ == '__main__':
     # plot_k_fields(gwf, pars,  k_fields, np.rad2deg(MF_Ensemble.ellipses[:,2]))
     if pars['printf']: print(f'Ensemble is initiated and respective k-fields are set in {(time.time() - start_time):.2f} seconds')
     #%% Running each model n times
-    start_time = time.time()
+    # start_time = time.time()
     
-    for idx in range(pars['nprern']):
-        MF_Ensemble.propagate()
-        MF_Ensemble.update_initial_heads()
-        VR_Model.simulation()
-        VR_Model.update_ic()
-        print(np.mean(VR_Model.get_field(['h'])['h']))
-    # print(MF_Ensemble.get_mean_var())
+    # for idx in range(pars['nprern']):
+    #     MF_Ensemble.propagate()
+    #     MF_Ensemble.update_initial_heads()
+    #     VR_Model.simulation()
+    #     VR_Model.update_ic()
+    #     print(np.mean(VR_Model.get_field(['h'])['h']))
+    # # print(MF_Ensemble.get_mean_var())
     
-    print(f'Each model is run and updated {pars["nprern"]} times which took {(time.time() - start_time):.2f} seconds')
-    print(f'That makes {((time.time() - start_time)/(pars["nprern"] * n_mem)):.2f} seconds per model run')
+    # print(f'Each model is run and updated {pars["nprern"]} times which took {(time.time() - start_time):.2f} seconds')
+    # print(f'That makes {((time.time() - start_time)/(pars["nprern"] * n_mem)):.2f} seconds per model run')
     
     #%%
     X, Ysim, _ = MF_Ensemble.get_Kalman_X_Y(pars['EnKF_p'])
@@ -173,23 +173,28 @@ if __name__ == '__main__':
     mean_obs = np.zeros((pars['nsteps'],len(obs_cid)))
     
 
-    Assimilate = True
     # for t_step in range(pars['nsteps']):
     for t_step in range(pars['nsteps']):
         if t_step == 0:
             MF_Ensemble.remove_current_files(pars)
-        if t_step == 2750:
+            
+        if (t_step*6/24) > pars['asim_d'][0] and (t_step*6/24) < pars['asim_d'][1]:
+            if t_step%4 == 0:
+                Assimilate = True
+            else:
+                Assimilate = False
+        else:
             Assimilate = False
                 
         print('--------')
         print(f'time step {t_step}')
-        start_time = time.time()
+        start_time_ts = time.time()
         rch_data, wel_data, riv_data = get_transient_data(pars, t_step)
         start_time = time.time()
         MF_Ensemble.update_transient_data(rch_data, wel_data, riv_data)
         VR_Model.update_transient_data(rch_data, wel_data, riv_data)
 
-        if pars['printf']: print(f'transient data loaded and applied in {(time.time() - start_time):.2f} seconds')
+        if pars['printf']: print(f'transient data loaded and applied in {(time.time() - start_time_ts):.2f} seconds')
         # print(MF_Ensemble.members[0].npf.k.array[0,542])
         # print(MF_Ensemble.members[0].rch.stress_period_data.get_data()[0][542])
         # print(MF_Ensemble.members[0].ic.strt.array[0,542])
@@ -226,9 +231,11 @@ if __name__ == '__main__':
         MF_Ensemble.model_error(true_h[t_step])
         MF_Ensemble.record_state(pars, pars['EnKF_p'], true_h[t_step])
         # visualize covariance structures
-        if pars['setup'] == 'office' and t_step%10 == 0:
+        if pars['setup'] == 'office' and Assimilate:
             if 'cov_data' in pars['EnKF_p']:
-                eigenvalues, eigenvectors = np.linalg.eig(MF_Ensemble.mean_cov_par)
+                m = MF_Ensemble.mean_cov_par
+                mat = np.array([[m[0], m[1]],[m[1], m[2]]])
+                eigenvalues, eigenvectors = np.linalg.eig(mat)
                 ellipses(
                     MF_Ensemble.ellipses,
                     pars['mat2cv'](eigenvalues, eigenvectors),
@@ -236,11 +243,12 @@ if __name__ == '__main__':
                     )
         
             
-            if t_step%50 == 20:
+            if t_step%40 == 20:
                 # k_fields = [Member.get_field(['npf'])['npf'].T for Member in MF_Ensemble.members[0:8]] 
                 # plot_k_fields(gwf, pars,  k_fields)
-                check_observations(true_obs[:t_step+1,:], mean_obs[:t_step+1,:], true_h[:t_step+1,:], mean_h[:t_step+1,:])
+                # check_observations(true_obs[:t_step+1,:], mean_obs[:t_step+1,:], true_h[:t_step+1,:], mean_h[:t_step+1,:])
                 compare_mean_true(gwf, [np.squeeze(VR_Model.npf.k.array), MF_Ensemble.meanlogk]) 
             
         if pars['printf']: print(f'Plotting and recording took {(time.time() - start_time):.2f} seconds')
+        if pars['printf']: print(f'Entire Step took {(time.time() - start_time_ts):.2f} seconds')
     

main/objects/Ensemble.py
@@ -116,7 +116,7 @@ class Ensemble:
         
         if self.pilotp_flag:
             start_time = time.time()
-            result = Parallel(n_jobs=self.nprocs, #!!!!!!!!!!!!!!
+            result = Parallel(n_jobs=self.nprocs, 
                               backend="threading")(
                                   delayed(self.members[idx].kriging)(
                                       params, 
@@ -238,21 +238,17 @@ class Ensemble:
     def model_error(self,  true_h):
         
         mean_h, var_h = self.get_mean_var()
-        true_h = np.array(true_h).flatten()
+        true_h = np.array(true_h).squeeze()
         
         # analogous for ole
         mean_obs = mean_h[self.obs_cid]
         true_obs = true_h[self.obs_cid]
         self.obs = [true_obs, mean_obs]
         
-        self.ole_nsq.append(np.sum(np.square(true_obs - mean_obs)/0.1)/mean_obs.size)
-        
-        ole = 0
-        for i in range(len(self.ole_nsq)):
-            ole += self.ole_nsq[i] 
+        self.ole_nsq.append(np.sum(np.square(true_obs - mean_obs)/0.01**2)/mean_obs.size)
         
         # ole for the model up until the current time step
-        self.ole.append(np.sqrt(ole/len(self.ole_nsq)))
+        self.ole.append(np.sqrt(np.sum(self.ole_nsq)/len(self.ole_nsq)))
         
         # calculating nrmse without root for later summation
         true_h = true_h[~self.h_mask]
@@ -261,17 +257,11 @@ class Ensemble:
         var_te2 = (true_h + mean_h)/2
         
         self.te1_nsq.append(np.sum(np.square(true_h - mean_h)/var_h))
-        self.te2_nsq.append(np.sum(np.square(true_h - mean_h)/var_te2))
-        
-        te1 = 0
-        te2 = 0
-        for i in range(len(self.te1_nsq)):
-            te1 += self.te1_nsq[i]
-            te2 += self.te2_nsq[i]
+        self.te2_nsq.append(np.sum(np.square(true_h - mean_h)/var_te2**2))
         
         # nrmse for the model up until the current time step
-        self.te1.append(np.sqrt(te1/len(self.te1_nsq)/mean_h.size))
-        self.te2.append(np.sqrt(te2/len(self.te2_nsq)/mean_h.size))
+        self.te1.append(np.sqrt(np.sum(self.te1_nsq)/len(self.te1_nsq)/mean_h.size))
+        self.te2.append(np.sqrt(np.sum(self.te2_nsq)/len(self.te2_nsq)/mean_h.size))
     
     def get_member_fields(self, params):
         
@@ -285,23 +275,11 @@ class Ensemble:
         
 
     def get_mean_var(self):
-        h_fields = []
-        for member in self.members:
-            h_fields.append(member.get_field(['h'])['h'].flatten())
+        h_fields = self.get_member_fields(['h'])
         
-        mean_h = np.zeros_like(h_fields[0])
-        var_h = np.zeros_like(h_fields[0])
-        count = 0
+        h_f = np.array([np.squeeze(field['h']) for field in h_fields]).T
         
-        for field in h_fields:
-            mean_h += field
-            var_h += np.square(field)
-            count += 1
-            
-        mean_h = mean_h/count
-        var_h = (var_h / count) - np.square(mean_h)
-        
-        return mean_h, var_h
+        return np.mean(h_f, axis = 1), np.var(h_f, axis = 1)
     
     def record_state(self, pars: dict, params: list, true_h):
         
@@ -365,16 +343,16 @@ class Ensemble:
             f.close()
             
             f = open(os.path.join(direc, 'covariance_data_par.dat'),'a')
-            f.write("{:.10f} ".format(self.mean_cov_par[0,0]))
-            f.write("{:.10f} ".format(self.mean_cov_par[1,1]))
-            f.write("{:.10f} ".format(self.mean_cov_par[0,1]))
+            f.write("{:.10f} ".format(self.mean_cov_par[0]))
+            f.write("{:.10f} ".format(self.mean_cov_par[1]))
+            f.write("{:.10f} ".format(self.mean_cov_par[2]))
             f.write('\n')
             f.close()
             
             f = open(os.path.join(direc, 'cov_variance_par.dat'),'a')
-            f.write("{:.10f} ".format(self.var_cov_par[0,0]))
-            f.write("{:.10f} ".format(self.var_cov_par[1,1]))
-            f.write("{:.10f} ".format(self.var_cov_par[0,1]))
+            f.write("{:.10f} ".format(self.var_cov_par[0]))
+            f.write("{:.10f} ".format(self.var_cov_par[1]))
+            f.write("{:.10f} ".format(self.var_cov_par[2]))
             f.write('\n')
             f.close()
             

main/objects/MFModel.py
@@ -141,7 +141,7 @@ class MFModel:
                 elif self.n_neg_def == 1:
                     print('A new model got stuck')
                     
-            return [[l1, l2, angle%np.pi], self.ellips_mat, success]
+            return [[l1, l2, angle%np.pi], [self.ellips_mat[0,0], self.ellips_mat[1,0], self.ellips_mat[1,1]], success]
                 
         else:
             pp_k = data