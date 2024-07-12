found another crucial mistake - dont confuse deviations with variance

main/dependencies/create_k_fields.py
@@ -2,12 +2,11 @@ import numpy as np
 from dependencies.conditional_k import conditional_k
 from dependencies.Kriging import Kriging
 
-def create_k_fields(gwf, pars: dict, pp_xy = [], pp_cid = [], test_cov = [], conditional = True, random = True):
+def create_k_fields(gwf, pars: dict, k_ref, pp_xy = [], pp_cid = [], test_cov = [], conditional = True, random = True):
     
     clx     = pars['lx']
     angles  = pars['ang']
     sigma   = pars['sigma'][0]
-    k_ref   = np.loadtxt(pars['k_r_d'], delimiter = ',')
     dx      = pars['dx']
     
     mg = gwf.modelgrid
@@ -57,7 +56,7 @@ def create_k_fields(gwf, pars: dict, pp_xy = [], pp_cid = [], test_cov = [], con
     
     #correcting a few pilot points if measurements are available
     if pars['f_meas']:
-        true_ppk = np.log(k_ref[pp_cid.astype(int)]) 
+        true_ppk = np.log(np.squeeze(k_ref)[pp_cid.astype(int)]) 
         true_ppk_pert = true_ppk + 0.05 * np.random.randn(*pp_k.shape) * true_ppk
         pp_k[pars['f_m_id']] = true_ppk_pert[pars['f_m_id']]
 

main/dependencies/get_transient_data.py
@@ -11,13 +11,22 @@ def get_transient_data(pars: dict, t_step: int):
     rivh  = np.genfromtxt(pars['rh_d'],delimiter = ',', names=True)['Wert']
 
     rch_data = abs(np.array(r_ref).flatten()) * sfac[t_step]
-    wel_data = np.zeros(5)
-    time = 0.25 * t_step
-    
-    for i in range(len(welq)):
-        if welst[i] <= time%365 and welnd[i] > time%365:
-            wel_data[i] = -welq[i]
     riv_data = rivh[t_step]
     
+    # check if we need to set up new wel files
+    day = 0.25 * t_step
+    
+    if int(day) in pars['welst'] or int(day) in pars['welnd']:
         
-    return rch_data, wel_data, riv_data
+        wel_data = np.zeros(5)
+        for i in range(len(welq)):
+            if welst[i] <= day and welnd[i] > day:
+                wel_data[i] = -welq[i]
+                
+        return [rch_data, riv_data, wel_data], ['rch', 'riv', 'wel']
+    else:
+        return [rch_data, riv_data], ['rch', 'riv']
+    
+    
+        
+    

main/dependencies/model_params.py
@@ -121,20 +121,20 @@ def get():
     q_idx       = [27, 9, 31, 5, 15]
     mask        = np.full(len(well_loc),True,dtype=bool)
     mask[q_idx] = False
-    years = 2
+    years = 1
     
     cov_mods    = ['Exponential', 'Matern', 'Gaussian']
     computer = ['office', 'binnac']
     setup = computer[0]
     if setup == 'office':
-        n_mem  = 40
+        n_mem  = 140
         nprocs = np.min([n_mem, psutil.cpu_count()])
         inspection = False
         printf = True
         if years == 1:
-            asimdays = [10, 300]
+            asimdays = [50, 300]
         elif years == 2:
-            asimdays = [10, 665]
+            asimdays = [50, 665]
         up_temp = True
         
         if n_mem == 2:
@@ -157,12 +157,12 @@ def get():
     cov_variants = [['cov_data', 'npf'], ['cov_data'], ['npf']]
     est_variants = ["underestimate", "good", "overestimate"]
     
-    nPP = 45
+    nPP = 40
     
     conditional_flag = False
-    field_meas_flag = True
+    field_meas_flag = False
     pilot_point_even = False
-    scramble_pp = False
+    scramble_pp = True
     
     l_red = 2
     h_damp = 0.6
@@ -189,8 +189,11 @@ def get():
             valtype = "random"
 
     if field_meas_flag:
-        np.random.seed(42)
-        meas_loc = np.random.randint(0, nPP, 5)
+        np.random.seed(85)
+        # meas_loc = np.random.randint(0, nPP, 7)
+        meas_loc = np.random.choice(np.arange(0, nPP), np.min([nPP,5]), replace=False)
+    else:
+        meas_loc = []
     
         
     pars    = {
@@ -235,7 +238,7 @@ def get():
         'chd'   : np.array([[0.0,2500], [5000,2500]]),      # start / end of river
         'chdh'  : 15,                                       # initial stage of riv
         'ss'    : 1e-5,                                     # specific storage
-        'sy'    : 0.15,                                     # specific yield
+        'sy'    : 0.4,                                      # specific yield
         'asim_d': asimdays,
         'mname' : "Reference",
         'sname' : "Reference",

main/dependencies/plotting/compare_mean.py
@@ -29,7 +29,7 @@ def compare_mean_true(gwf, k_fields, poi):
 
     # Plot lower
     ax2 = axes[2]
-    gwf.npf.k.set_data((k_fields[1])/ np.log((k_fields[0])))
+    gwf.npf.k.set_data(k_fields[2])
     axf2 = flopy.plot.PlotMapView(model=gwf, ax=ax2)
     c2 = axf2.plot_array((gwf.npf.k.array), cmap=cm.roma, alpha=1)
     ax2.set_aspect('equal')
@@ -50,7 +50,8 @@ def compare_mean_true(gwf, k_fields, poi):
     
     # Set custom bounds for colorbars
     cbar0.mappable.set_clim(vmin=kmin, vmax=kmax)
-    cbar1.mappable.set_clim(vmin=0.5, vmax=1.5)
+    # cbar1.mappable.set_clim(vmin=0, vmax=1.5)
+    # cbar1.mappable.set_clim(vmin=0.5, vmax=1.5)
     
     if poi.any():
         ax0.scatter(poi[:,0], poi[:,1], c = 'black', marker = 'x', s = 3)

main/dependencies/shoutout_difference.py
@@ -2,5 +2,10 @@ import numpy as np
 def shout_dif(array1, array2):
     
     meandiff = np.mean(np.abs(array1-array2))
+    maxdiff = np.max(np.abs(array1-array2))
+    mindiff = np.min(np.abs(array1-array2))
     
-    print(f"The average difference between observations is {meandiff}")
+    print(f"Average difference: {meandiff:.4f}")
+    print(f"Maximum difference: {maxdiff:.4f}")
+    print(f"Minimum difference: {mindiff:.4f}")
+    

main/main.py
@@ -9,9 +9,10 @@ from dependencies.get_transient_data import get_transient_data
 from dependencies.intersect_with_grid import intersect_with_grid
 from dependencies.generate_mask import chd_mask
 from dependencies.plotting.ellipses import ellipses
-from dependencies.plotting.compare_mean import compare_mean_true
+from dependencies.plotting.compare_mean import compare_mean_true 
+from dependencies.plotting.compare_mean_h import compare_mean_true_head
 from dependencies.plotting.check_observations import check_observations
-# from dependencies.plotting.plot_k_fields import plot_k_fields
+from dependencies.plotting.poi import plot_POI
 from dependencies.shoutout_difference import shout_dif
 from dependencies.plotting.plot_k_fields import plot_k_fields
 from objects.Ensemble import Ensemble
@@ -77,6 +78,7 @@ if __name__ == '__main__':
         result = Parallel(n_jobs=nprocs, backend = "threading")(delayed(create_k_fields)(
             gwf,
             pars, 
+            VR_Model.npf.k.array,
             pp_xy,
             pp_cid,
             conditional = pars['condfl']
@@ -107,10 +109,10 @@ if __name__ == '__main__':
             cor_ellips.append([])
             l_angs.append([])
             pp_xy, pp_cid = [], []
-        
+
     # save original fields
-    if pars['setup'] == 'binnac':
-        np.save(os.path.join(pars['resdir'] ,'k_ensemble_ini.npy'), k_fields)
+    # if pars['setup'] == 'binnac':
+    #     np.save(os.path.join(pars['resdir'] ,'k_ensemble_ini.npy'), k_fields)
     
     mask_chd = chd_mask(gwf)
     
@@ -179,10 +181,10 @@ if __name__ == '__main__':
         print(f'time step {t_step}')
         start_time_ts = time.time()
         if t_step%4 == 0:
-            rch_data, wel_data, riv_data = get_transient_data(pars, t_step)
+            data, packages = get_transient_data(pars, t_step)
 
-            MF_Ensemble.update_transient_data(rch_data, wel_data, riv_data)
-            VR_Model.update_transient_data(rch_data, wel_data, riv_data)
+            MF_Ensemble.update_transient_data(data, packages)
+            VR_Model.update_transient_data(data, packages)
 
             if pars['printf']: print(f'transient data loaded and applied in {(time.time() - start_time_ts):.2f} seconds')
         
@@ -210,7 +212,7 @@ if __name__ == '__main__':
             MF_Ensemble.apply_X(EnKF.X, pars['EnKF_p'])
             
             
-            interim = [int(i+48-100) for i in obs_cid]
+            interim = [int(i+pars['n_PP']+3-100) for i in obs_cid]
             shout_dif(true_obs[t_step,:], np.mean(EnKF.X, axis = 1)[interim])
 
             if pars['printf']: print(f'Application of results plus kriging took {(time.time() - start_time):.2f} seconds')
@@ -225,7 +227,7 @@ if __name__ == '__main__':
         if period == "assimilation" or period == "prediction":
             if t_step%4 == 0:
 
-                var_h = MF_Ensemble.model_error(true_h, period)
+                mean_h, var_h = MF_Ensemble.model_error(true_h, period)
                 MF_Ensemble.record_state(pars, pars['EnKF_p'], np.squeeze(true_h), period)
             
                 # visualize covariance structures
@@ -238,8 +240,9 @@ if __name__ == '__main__':
                             pars['mat2cv'](mat),
                             pars
                             )
-                    if t_step%60 == 0:
-                        compare_mean_true(gwf, [np.squeeze(VR_Model.npf.k.array), MF_Ensemble.meanlogk, var_h], pp_xy[pars['f_m_id']]) 
+                    if t_step%20 == 0:
+                        compare_mean_true(gwf, [np.squeeze(VR_Model.npf.k.array), MF_Ensemble.meanlogk, MF_Ensemble.varlogk], pp_xy[pars['f_m_id']])
+                        compare_mean_true_head(gwf, [np.squeeze(true_h), np.squeeze(mean_h), np.squeeze(var_h)], pp_xy[pars['f_m_id']]) 
                 
                 if pars['printf']: print(f'Plotting and recording took {(time.time() - start_time):.2f} seconds')
                 if pars['printf']: print(f'Entire Step took {(time.time() - start_time_ts):.2f} seconds')

main/objects/Ensemble.py
@@ -20,6 +20,7 @@ class Ensemble:
         self.obs_cid    = [int(i) for i in obs_cid]
         self.meanlogk   = []
         self.meank   = []
+        self.vark   = []
         if pilotp_flag:
             self.ellipses   = ellipses
             self.ellipses_par = ellipses_par
@@ -67,7 +68,7 @@ class Ensemble:
                 
                 if pars['f_meas']:
                     ids = cl + pars['f_m_id']
-                    damp[ids] = val[2] / 10
+                    damp[ids] = val[2] / 50
         else:
             if self.pilotp_flag:
                 damp[:len(self.pp_cid)] = val[1]
@@ -136,22 +137,27 @@ class Ensemble:
         Ysim = np.vstack(ysims).T
         return X, Ysim
     
-    def update_transient_data(self, rch_data, wel_data, riv_data):
+    def update_transient_data(self, data, packages):
 
-        spds = self.members[0].get_field(['rch', 'wel', 'riv'])
+        spds = self.members[0].get_field(packages)
         rch_spd = spds['rch']
-        wel_spd = spds['wel']
         riv_spd = spds['riv']
         rivhl = np.ones(np.shape(riv_spd[0]['cellid']))
-        rch_spd[0]['recharge'] = rch_data
-        riv_spd[0]['stage'] = rivhl * riv_data
-        wel_spd[0]['q'] = wel_data
+        
+        rch_spd[0]['recharge'] = data[0]
+        riv_spd[0]['stage'] = rivhl * data[1]
+        
+        if 'wel' in packages:
+            wel_spd = spds['wel']
+            wel_spd[0]['q'] = data[2]
+            spds = [rch_spd, riv_spd, wel_spd]
+        else:
+            spds = [rch_spd, riv_spd]
 
         
-        # this is the part that takes the most time 
         Parallel(n_jobs=self.nprocs)(delayed(self.members[idx].set_field)(
-            [rch_spd, wel_spd, riv_spd],
-            ['rch', 'wel', 'riv']
+            spds,
+            packages
             ) 
             for idx in range(self.n_mem)
             )
@@ -162,28 +168,38 @@ class Ensemble:
         mean_h, var_h = self.get_mean_var(h = 'ic')
         true_h = np.squeeze(true_h)
         
-        # analogous for ole
         mean_obs = mean_h[self.obs_cid]
         true_obs = true_h[self.obs_cid]
         self.obs = [true_obs, mean_obs]
-
-        self.ole_nsq[period].append(np.sum(np.square(true_obs - mean_obs)/0.01**2)/mean_obs.size)
-        
-        # ole for the model up until the current time step
-        self.ole[period].append(np.sqrt(np.sum(self.ole_nsq[period])/len(self.ole_nsq[period])))
         
         # calculating nrmse without root for later summation
-        true_h = true_h[~self.h_mask]
-        mean_h = mean_h[~self.h_mask]
-        var_h = var_h[~self.h_mask]
+        true_h_m = true_h[~self.h_mask]
+        mean_h_m = mean_h[~self.h_mask]
+        var_h_m = var_h[~self.h_mask]
         var_te2 = (true_h + mean_h)/2
         
-        self.te1_nsq[period].append(np.sum(np.square(true_h - mean_h)/var_h))
-        self.te2_nsq[period].append(np.sum(np.square(true_h - mean_h)/var_te2**2))
         
-        # nrmse for the model up until the current time step
-        self.te1[period].append(np.sqrt(np.sum(self.te1_nsq[period])/len(self.te1_nsq[period])/mean_h.size))
-        self.te2[period].append(np.sqrt(np.sum(self.te2_nsq[period])/len(self.te2_nsq[period])/mean_h.size))
+        # Computing normalized squared error only considering nodes
+        node_ole = np.mean(true_obs - mean_obs)**2/(0.01**2)
+        node_te1 = np.mean(true_h_m - mean_h_m)**2/var_h_m
+        node_te2 = np.mean(true_h_m - mean_h_m)**2/(var_te2**2)
+        
+        # Append node error to list
+        self.ole_nsq[period].append(node_ole)
+        self.te1_nsq[period].append(node_te1)
+        self.te2_nsq[period].append(node_te2)
+
+        # Calculate NRMSE over all node calculations
+        time_ole = np.sqrt(np.mean(self.ole_nsq[period]))
+        time_te1 = np.sqrt(np.mean(self.te1_nsq[period]))
+        time_te2 = np.sqrt(np.mean(self.te2_nsq[period]))
+
+        # Append error to resulting list
+        self.ole[period].append(time_ole)
+        self.te1[period].append(time_te1)
+        self.te2[period].append(time_te2)
+        
+        return mean_h, var_h
     
     def get_member_fields(self, params):
         
@@ -209,6 +225,7 @@ class Ensemble:
         k_fields = self.get_member_fields(['npf'])
         k_fields = np.array([field['npf'] for field in k_fields]).squeeze()
         self.meanlogk = np.mean(np.log(k_fields), axis = 0)
+        self.varlogk = np.var(np.log(k_fields), axis = 0)
         self.meank = np.mean(k_fields, axis = 0)
         
         direc = pars['resdir']
@@ -312,10 +329,14 @@ class Ensemble:
                 g.close()
             
             f = open(os.path.join(direc,  'meanlogk.dat'),'a')
+            g = open(os.path.join(direc,  'varlogk.dat'),'a')
             for i in range(len(self.meanlogk)):
                 f.write("{:.8f} ".format(self.meanlogk[i]))
+                f.write("{:.8f} ".format(self.varlogk[i]))
             f.write('\n')
+            g.write('\n')
             f.close()
+            g.close()
             
             f = open(os.path.join(direc,  'meank.dat'),'a')
             for i in range(len(self.meank)):
@@ -338,7 +359,7 @@ class Ensemble:
                       os.path.join(pars['resdir'], 'obs_true.dat'),
                       os.path.join(pars['resdir'], 'meank.dat'),
                       os.path.join(pars['resdir'], 'meanlogk.dat'),
-                      os.path.join(pars['resdir'], 'k_mean.dat'),
+                      os.path.join(pars['resdir'], 'varlogk.dat'),
                       os.path.join(pars['resdir'], 'h_mean.dat'),
                       os.path.join(pars['resdir'], 'h_var.dat'),
                       os.path.join(pars['resdir'], 'obs_mean.dat'),

main/objects/EnsembleKalmanFilter.py
@@ -49,7 +49,7 @@ class EnsembleKalmanFilter:
         Y_prime = self.Ysim  - Ymean
         
         # Measurement uncertainty matrix
-        R       = np.identity(self.n_obs) * self.eps 
+        R       = np.identity(self.n_obs) * self.eps**2
         
         # Covariance matrix
         Cyy     = 1/(self.n_mem-1)*np.matmul((Y_prime),(Y_prime).T) + R 
@@ -63,7 +63,7 @@ class EnsembleKalmanFilter:
         Y_obs = np.tile(Y_obs, (self.n_mem,1)).T
         # perturb measurements
         # HERE COULD BE A SOURCE FOR LARGER ERRORS
-        Y_obs -= np.random.normal(loc=0, scale=np.sqrt(self.eps), size=Y_obs.shape)
+        Y_obs -= np.random.normal(loc=0, scale=self.eps, size=Y_obs.shape)
         self.X += 1/(self.n_mem-1) * (self.damp *
                     np.matmul(
                         self.X_prime, np.matmul(

main/objects/Virtual_Reality.py
@@ -69,21 +69,24 @@ class Virtual_Reality:
                 
         return fields
     
-    def update_transient_data(self,rch_data, wel_data, riv_data):
-        
-        spds = self.get_field(['rch', 'wel', 'riv'])
-        
-        rch_spd = spds['rch']
-        wel_spd = spds['wel']
-        riv_spd = spds['riv']
+    def update_transient_data(self, data, packages):
 
+        spds = self.get_field(packages)
+        rch_spd = spds['rch']
+        riv_spd = spds['riv']
         rivhl = np.ones(np.shape(riv_spd[0]['cellid']))
         
-        rch_spd[0]['recharge'] = rch_data
-        riv_spd[0]['stage'] = rivhl * riv_data
-        wel_spd[0]['q'] = wel_data
+        rch_spd[0]['recharge'] = data[0]
+        riv_spd[0]['stage'] = rivhl * data[1]
         
-        self.set_field([rch_spd, wel_spd, riv_spd],['rch', 'wel', 'riv']) 
+        if 'wel' in packages:
+            wel_spd = spds['wel']
+            wel_spd[0]['q'] = data[2]
+            spds = [rch_spd, riv_spd, wel_spd]
+        else:
+            spds = [rch_spd, riv_spd]
+        
+        self.set_field(spds, packages) 
         
     def simulation(self):
         success, buff = self.sim.run_simulation()